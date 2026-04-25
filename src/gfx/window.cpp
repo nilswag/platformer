3fe77dcc487af1c54798caadc333590fff618ddd
nilswag <nilswagemaker05@icloud.com>
2026-04-25 23:12:55 +0200

    Refactored window.cpp

--- src/gfx/window.cpp
+++ src/gfx/window.cpp
@@ -12,7 +12,7 @@ void Window::frameBufferSizeCallback(GLFWwindow* window, int width, int height)
 	instance.m_height = height;
 	glViewport(0, 0, width, height);
 
-	logger::debug("[Window] Resized to ({}, {})", width, height);
+	logger::debug("Window", "Resized to ({}, {})", width, height);
 }
 
 Window& Window::getInstance()
@@ -30,7 +30,7 @@ Window::Window()
 	m_dt = 0.0;
 
 	if (!glfwInit())
-		logger::fatal("[Window] Failed to initialize glfw");
+		logger::fatal("Window", "Failed to initialize glfw");
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
@@ -38,32 +38,32 @@ Window::Window()
 
 	m_window = glfwCreateWindow(m_width, m_height, m_title, nullptr, nullptr);
 	if (!m_window)
-		logger::fatal("[Window] Failed to initialize glfw window");
+		logger::fatal("Window", "Failed to initialize glfw window");
 	glfwMakeContextCurrent(m_window);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-		logger::fatal("[Window] Failed to initialize glad");
+		logger::fatal("Window", "Failed to initialize glad");
 
-	logger::info("[Window] GL Version: {}", (char*)glGetString(GL_VERSION));
-	logger::info("[Window] GL Vendor: {}", (char*)glGetString(GL_VENDOR));
-	logger::info("[Window] GL Renderer: {}", (char*)glGetString(GL_RENDERER));
+	logger::info("Window", "GL Version : {}", (char*)glGetString(GL_VERSION));
+	logger::info("Window", "GL Vendor: {}", (char*)glGetString(GL_VENDOR));
+	logger::info("Window", "GL Renderer: {}", (char*)glGetString(GL_RENDERER));
 
 	glfwSetFramebufferSizeCallback(m_window, frameBufferSizeCallback);
 	glViewport(0, 0, m_width, m_height);
 
-	logger::debug("[Window] Initialized ({}x{})", m_width, m_height);
+	logger::debug("Window", "Initialized ({}x{})", m_width, m_height);
 }
 
 Window::~Window()
 {
 	glfwDestroyWindow(m_window);
 	glfwTerminate();
-	logger::debug("[Window] Deinitialized");
+	logger::debug("Window", "Deinitialized");
 }
 
 void Window::loop()
 {
-	logger::trace("[Window] Loop begin");
+	logger::trace("Window", "Loop begin");
 
 	auto last = std::chrono::high_resolution_clock::now();
 	double timer = 0.0;
@@ -86,14 +86,14 @@ void Window::loop()
 
 		if (timer >= 1.0)
 		{
-			timer = 0.0;
+			timer -= 1.0;
 			double avgDt = sum / count;
 			count = 0;
 
 			int avgFps = static_cast<int>(1.0 / avgDt);
 			double avgMs = avgDt * 1e3;
 
-			logger::info("[Window] AVG FPS: {}, AVG DT: {}ms", avgFps, std::round(avgMs * 100.0) / 100.0);
+			logger::info("Window", "AVG FPS: {} | AVG DT: {}ms", avgFps, std::round(avgMs * 100.0) / 100.0);
 		}
 
 		glClear(GL_COLOR_BUFFER_BIT);
@@ -101,5 +101,5 @@ void Window::loop()
 		glfwSwapBuffers(m_window);
 	}
 
-	logger::trace("[Window] Loop end");
+	logger::trace("Window", "Loop end");
 }
