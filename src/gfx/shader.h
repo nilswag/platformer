8331c3c226322d0987de615d97a5eecd70c0409e
nilswag <nilswagemaker05@icloud.com>
2026-04-30 21:36:35 +0200

    Added glm and corresponding uniform setters

--- src/gfx/shader.h
+++ src/gfx/shader.h
@@ -3,6 +3,8 @@
 #include <string_view>
 #include <map>
 #include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 #include "../util/log.h"
 
@@ -15,17 +17,21 @@ public:
 	void use() const;
 
 	inline unsigned int id() const { return m_id; }
-	inline void setFloat(std::string& name, float value) { if (!hasUniform(name)) return; glUniform1f(m_uniforms[name], value); }
-	inline void setInt(std::string& name, int value) {     if (!hasUniform(name)) return; glUniform1i(m_uniforms[name], value); }
-	inline void setBool(std::string& name, bool value) {   if (!hasUniform(name)) return; glUniform1ui(m_uniforms[name], value); }
+	inline void setFloat(const std::string& name, float value) { if (!hasUniform(name)) return; glUniform1f(m_uniforms[name], value); }
+	inline void setInt(const std::string& name, int value) { if (!hasUniform(name)) return; glUniform1i(m_uniforms[name], value); }
+	inline void setBool(const std::string& name, bool value) { if (!hasUniform(name)) return; glUniform1i(m_uniforms[name], value); }
+	inline void setVec2(const std::string& name, const glm::vec2& value) { if (!hasUniform(name)) return; glUniform2fv(m_uniforms[name], 1, glm::value_ptr(value)); }
+	inline void setVec3(const std::string& name, const glm::vec3& value) { if (!hasUniform(name)) return; glUniform3fv(m_uniforms[name], 1, glm::value_ptr(value)); }
+	inline void setMat2x2(const std::string& name, const glm::mat2x2& value) { if (!hasUniform(name)) return; glUniformMatrix2fv(m_uniforms[name], 1, GL_FALSE, glm::value_ptr(value)); }
+	inline void setMat3x3(const std::string& name, const glm::mat3x3& value) { if (!hasUniform(name)) return; glUniformMatrix3fv(m_uniforms[name], 1, GL_FALSE, glm::value_ptr(value)); }
 
 private:
 	bool compileShader(int& id, std::string_view shaderSrc, unsigned int type) const;
 
-	inline bool hasUniform(std::string& name)
+	inline bool hasUniform(const std::string& name)
 	{
 		if (m_uniforms.contains(name)) return true;
-		log().err(m_tag, "Uniform not found ({})", name);
+		log().warn(m_tag, "Uniform not found ({})", name);
 		return false;
 	}
 
