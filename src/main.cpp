d79dbd2b42c7479816c4af0d9745d3442ee2aefd
nilswag <nilswagemaker05@icloud.com>
2026-05-02 01:26:58 +0200

    Added the log level switch based on debug mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
+#include "util/log.h"
 #include "app.h"
 
 int main()
 {
+#ifdef _DEBUG
+	log().setLevel(LogLevel::TRACE);
+#else
+	log().setLevel(LogLevel::FATAL);
+#endif
+
 	App app;
 	app.run();
 
